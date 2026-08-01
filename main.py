acad8ac45f57a9a06a6752910c9f7849defbc9e6
Suhani Sinha <suhanisinha006@gmail.com>
2026-08-01 20:07:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# -----------------------------
+# Classes
+# -----------------------------
+
+class GSTCategory:
+    # Class variable (shared by all instances)
+    default_rates = {
+        "essential goods": 0,
+        "clothing & footwear": 10,
+        "food items": 5,
+        "electronics": 18,
+        "luxury goods": 28,
+        "education": 0,
+        "healthcare": 5,
+        "automobile": 28,
+        "entertainment services": 22,
+        "personal care products": 5
+        
+     }
