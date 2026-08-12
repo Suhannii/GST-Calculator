c247d13b364350fa0582300966cd897d4f9e18fb
Suhani Sinha <suhanisinha006@gmail.com>
2026-08-12 20:28:37 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,3 +2,21 @@ from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import os
 
+# ----------------------------------------------------
+# 1. Core GST Calculation Logic (From your main.py)
+# ----------------------------------------------------
+
+class GSTCategory:
+    default_rates = {
+        "essential goods": 0,
+        "clothing & footwear": 10,
+        "food items": 5,
+        "electronics": 18,
+        "luxury goods": 28,
+        "education": 0,
+        "healthcare": 5,
+        "automobile": 28,
+        "entertainment services": 22,
+        "personal care products": 5
+        
+    }
